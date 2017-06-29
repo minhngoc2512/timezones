71fc57583a5691b25e1020b14fa521266f67270a
minhngoc2512 <minhngoc2512@yahoo.com>
2017-06-29 12:19:46 +0700

    TimzonesServiceProvider.php
    minh

--- src/TimzonesServiceProvider.php
+++ src/TimzonesServiceProvider.php
@@ -16,7 +16,14 @@ class TimzonesServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__.'/views','timezones');
         $this->publishes([
             __DIR__.'/views' => base_path('resources/views/timezones'),
+            __DIR__.'/TimezonesController.php' => base_path('app/Http/Controllers'),
+
         ]);
+        file_put_contents(
+            base_path('routes/web.php'),
+            file_get_contents(__DIR__.'/routes.php'),
+            FILE_APPEND
+        );
         //
     }
 
